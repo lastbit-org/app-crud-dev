9db510d2eef59725254b655858d59ee5718ec107
eduardo239 <eucrieiumaconta@gmail.com>
2026-03-22 16:08:22 -0300

    chore: add initial project dependencies and configuration files for frontend and backend.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+
+import './css/App.css'
+
+function App() {
+  const [data, setData] = useState("")
+
+
+  const getData =async () => {
+
+    const res = await fetch('http://localhost:3000/api/data')
+    const response = await res.json()
+    console.log(response)
+    setData(response.message)
+
+  }
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+        </div>
+        <button
+          className="counter"
+          onClick={() => getData()}
+        >
+          Get Data
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer">
+        {data}
+      </section>
+    </>
+  )
+}
+
+export default App
