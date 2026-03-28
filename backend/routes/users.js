44a4f159c1845cf56b89bb7e3be50265141e72ad
eduardo239 <eucrieiumaconta@gmail.com>
2026-03-28 09:51:53 -0300

    feat: add test endpoint to users route and update code style formatting

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/test", (req, res) => {
+  res.json({ message: "Test route works!" });
 });
 
 module.exports = router;
